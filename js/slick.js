d337edbddee3416db94d080f60a3079b671a9617
ivan1810 <78267912+IvanRobak@users.noreply.github.com>
2023-12-12 14:20:25 +0200

    slider

---
+++ js/slick.js
@@ -0,0 +1,31 @@
+$('.responsive').slick({
+  // dots: true,
+  // prevArrow: '<button type="button" class="my-slick-prev">&xlarr;</button>',
+  // nextArrow: '<button type="button" class="my-slick-next">&xrarr;</button>',
+  // infinite: true,
+  // speed: 300,
+  // slidesToScroll: 1,
+  // adaptiveHeight: true,
+  // autoplay: true,
+  // autoplaySpeed: 3000,
+  slidesToShow: 1,
+  dots: true,
+
+  // responsive: [
+  //   {
+  //     breakpoint: 1200,
+  //     settings: {
+  //       slidesToShow: 1,
+  //     },
+  //   },
+  //   {
+  //     breakpoint: 768,
+  //     settings: {
+  //       slidesToShow: 1,
+  //     },
+  //   },
+  //   // You can unslick at a given breakpoint now by adding:
+  //   // settings: "unslick"
+  //   // instead of a settings object
+  // ],
+});
